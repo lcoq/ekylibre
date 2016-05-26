ea8dfb48c22819da3df02881b3ce894653c5b279
Louis Coquio <louis.coquio@gmail.com>
2016-05-26 14:54:02 +0200

    Clean bank statement items controller

--- app/controllers/backend/bank_statement_items_controller.rb
+++ app/controllers/backend/bank_statement_items_controller.rb
@@ -2,22 +2,8 @@ module Backend
   class BankStatementItemsController < Backend::BaseController
     def new
       @bank_statement_item = BankStatementItem.new
-      if params[:debit]
-        @bank_statement_item.debit = params[:debit]
-      end
-      if params[:credit]
-        @bank_statement_item.credit = params[:credit]
-      end
-      if params[:transfered_on]
-        @bank_statement_item.transfered_on = params[:transfered_on]
-      end
-      if params[:letter]
-        @bank_statement_item.letter = params[:letter]
-      end
-      if params[:bank_statement_id]
-        bank_statement = BankStatement.find_by(id: params[:bank_statement_id])
-        @bank_statement_item.bank_statement = bank_statement
-      end
+      permitted_params = params.permit(:debit, :credit, :transfered_on, :letter, :bank_statement_id)
+      @bank_statement_item.attributes = permitted_params
       if request.xhr?
         if params[:reconciliation]
           render partial: "backend/bank_statement_items/reconciliation_row_form", object: @bank_statement_item
