15fa8e5888e07617c5b79dc01a1145467a8484de
Louis Coquio <louis.coquio@gmail.com>
2016-10-14 09:38:34 -0400

    Prevent Cash creation with a journal which is not a bank nor cash

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -77,7 +77,7 @@ class Journal < Ekylibre::Record::Base
   scope :various,   -> { where(nature: 'various') }
   scope :cashes,    -> { where(nature: 'cashes') }
   scope :stocks,    -> { where(nature: 'stocks') }
-  scope :banks_or_cashes, -> { where(nature: 'cashes').or.where(nature: 'bank') }
+  scope :banks_or_cashes, -> { where(nature: ['cashes', 'bank']) }
 
   before_validation(on: :create) do
     if year = FinancialYear.first_of_all
