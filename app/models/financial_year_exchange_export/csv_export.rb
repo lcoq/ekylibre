5fda57172f29f5cd6cbe0009a75370d4e40c6473
Louis Coquio <louis.coquio@gmail.com>
2016-11-23 09:50:41 +0100

    Clean FinancialYearExchangeExport::CsvExport

--- app/models/financial_year_exchange_export/csv_export.rb
+++ app/models/financial_year_exchange_export/csv_export.rb
@@ -22,14 +22,35 @@ class FinancialYearExchangeExport
         csv << headers
         exchange.journal_entries.includes(:items).order(printed_on: :desc).each do |entry|
           entry.items.each do |entry_item|
-            csv << [ entry.printed_on, entry_item.account.number, entry.journal.code, entry_item.third_party.try(:full_name), entry.number, entry_item.name, entry_item.absolute_debit, entry_item.absolute_credit, entry_item.letter ]
+            csv << [
+              entry.printed_on,
+              entry_item.account.number,
+              entry.journal.code,
+              entry_item.third_party.try(:full_name),
+              entry.number,
+              entry_item.name,
+              entry_item.absolute_debit,
+              entry_item.absolute_credit,
+              entry_item.letter
+            ]
           end
         end
       end
     end
 
     def headers
-      [ 'Jour', 'Numéro de compte', 'Journal', 'Tiers', 'Numéro de pièce', 'Libellé écriture', 'Débit', 'Crédit', 'Lettrage' ]
+      [
+        'Jour',
+        'Numéro de compte',
+        'Journal',
+        'Tiers',
+        'Numéro de pièce',
+        'Libellé écriture',
+        'Débit',
+        'Crédit',
+        'Lettrage'
+      ]
     end
+
   end
 end
